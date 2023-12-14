69325a488e6096fff8201046fdba4f4dbff5511a
liton61 <litonofficial61@gmail.com>
2023-12-14 22:38:03 +0600

    services page added

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,7 +3,7 @@
 const Contact = () => {
     return (
         <div className="lg:w-3/4 w-full mx-auto my-20">
-            <h1 className="text-2xl font-medium text-center border-b-2 border-success w-72 mx-auto">Contact with us</h1>
+            <h1 className="text-2xl font-medium text-center border-b-2 border-success w-60 mx-auto">Contact with us</h1>
             <form className="bg-white shadow-md rounded px-5 pt-6 pb-8 mb-4">
                 <div className="mb-4">
                     <label className="block text-gray-700 text-sm font-bold mb-2">
--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -1,9 +1,22 @@
+import { useEffect, useState } from "react";
+import ServicesCard from "./ServicesCard";
 
 
 const Services = () => {
+    const [services, setServices] = useState([]);
+    useEffect(() => {
+        fetch('services.json')
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, [])
     return (
-        <div>
+        <div className="lg:w-3/4 mx-auto">
             <h1>Services</h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:p-0 md:p-0 p-5">
+                {
+                    services.map(service => <ServicesCard key={service._id} service={service}></ServicesCard>)
+                }
+            </div>
         </div>
     );
 };
