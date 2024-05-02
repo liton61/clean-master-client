b84b0f77aae2362ab83bd9fddca693ae048c422b
liton61 <litonofficial61@gmail.com>
2024-05-02 15:16:42 +0600

    diff section added

---
+++ src/components/Diff/Diff.jsx
@@ -0,0 +1,48 @@
+import before from '../../assets/before.jpg';
+import after from '../../assets/after.jpg';
+
+const Diff = () => {
+    return (
+        <div className='lg:w-3/4 mx-auto lg:px-0 px-5 my-20'>
+            <div className='grid lg:grid-cols-2 gap-10'>
+                <div className="diff aspect-[6/4]">
+                    <div className="diff-item-1">
+                        <div className="bg-primary text-primary-content text-9xl font-black grid place-content-center">
+                            <img src={after} alt="" />
+                        </div>
+                    </div>
+                    <div className="diff-item-2">
+                        <div className="bg-base-200 text-9xl font-black grid place-content-center">
+                            <img src={before} alt="" />
+                        </div>
+                    </div>
+                    <div className="diff-resizer"></div>
+                </div>
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-5'>
+                    <div className='bg-base-200 rounded py-5'>
+                        <i className="fa-regular fa-heart text-5xl flex justify-center text-green-600"></i>
+                        <h1 className='text-3xl font-bold text-center'>870+</h1>
+                        <p className='text-center font-medium'>Happy Customers</p>
+                    </div>
+                    <div className='bg-base-200 rounded py-5'>
+                        <i className="fa-solid fa-broom text-5xl flex justify-center text-green-600"></i>
+                        <h1 className='text-3xl font-bold text-center'>890+</h1>
+                        <p className='text-center font-medium'>Projects Completed</p>
+                    </div>
+                    <div className='bg-base-200 rounded py-5'>
+                        <i className="fa-solid fa-gift text-5xl flex justify-center text-green-600"></i>
+                        <h1 className='text-3xl font-bold text-center'>483+</h1>
+                        <p className='text-center font-medium'>Year established</p>
+                    </div>
+                    <div className='bg-base-200 rounded py-5'>
+                        <i className="fa-solid fa-truck text-5xl flex justify-center text-green-600"></i>
+                        <h1 className='text-3xl font-bold text-center'>678+</h1>
+                        <p className='text-center font-medium'>Countries reached</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Diff;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import About from "../../components/About/About";
 import Banner from "../../components/Banner/Banner";
 import Contact from "../../components/Contact/Contact";
+import Diff from "../../components/Diff/Diff";
 import Estimate from "../../components/Estimate/Estimate";
 import Kitchen from "../../components/Kitchen/Kitchen";
 // import Room from "../../components/Room/Room";
@@ -15,6 +16,7 @@ const Home = () => {
             <About></About>
             <Services></Services>
             <Estimate></Estimate>
+            <Diff></Diff>
             {/* <Room></Room> */}
             <RoomBanner></RoomBanner>
             <Kitchen></Kitchen>
